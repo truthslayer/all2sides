40e7f436c281e083410d7c24853371ba110e8c69
Jamie M <truthslayer@users.noreply.github.com>
2017-10-17 10:10:48 -0400

    deleting some stuff

--- s3web/js/swipes.js
+++
@@ -1,8 +0,0 @@
-window.mySwipe = Swipe(document.getElementById('slider'), {
-    continuous: true,
-    callback: function(index, element) {
-	console.log('index' + index + ' element ' + element);
-	    get_loaded(element);
-	},
-    });
-
